367c6b8a2b983902316cfbd524d7d7199d4da04b
vevis <vevisvs@gmail.com>
2023-07-15 19:15:55 -0300

    agregando validaciones al form y mensajes de error en la vista

--- src/app/board/pages/users/components/modal-users/modal-users.component.ts
+++ src/app/board/pages/users/components/modal-users/modal-users.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, EventEmitter } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Users } from '../../models/users';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Output } from '@angular/core'
 
 @Component({
@@ -11,10 +11,6 @@ import { Output } from '@angular/core'
 })
 
 export class ModalUsersComponent  {
-  // @Output() dataFormUser = new EventEmitter();
-
-  // public dataUser: any = [] //variable para almacenar los datos del form
-
   constructor(
     public dialogRef: MatDialogRef<ModalUsersComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Users,
@@ -22,11 +18,11 @@ export class ModalUsersComponent  {
   ) { }
 
   formUser = this.fb.group({
-    name: [''],
-    lastname: [''],
-    email: [''],
-    country: [''],
-    phone: []
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    lastname: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, Validators.email]],
+    country: ['', [Validators.required]],
+    phone: ['' ,[Validators.required, Validators.pattern('^[0-9]+$')]]
   })
 
   cancelar(): void {
@@ -37,4 +33,26 @@ export class ModalUsersComponent  {
     this.dialogRef.close(this.formUser.value); //cierra el form y envia su valor
   }
 
+  //funcion auxiliar para manejar errores del form y enviar mensaje al usuario:
+  public getError(inputName: string): string {
+    let error = '';
+    const control = this.formUser.get(inputName);
+    if (control?.touched && control.errors) {
+      const errorObj = control.errors;
+      if (errorObj['required']) {
+        error = 'Este campo es obligatorio';
+      } else if (errorObj['minlength'] && inputName === 'name') {
+        error = 'Este campo debe tener como mínimo 3 caracteres';
+      } else if (errorObj['minlength'] && inputName === 'lastname') {
+        error = 'Este campo debe tener como mínimo 2 caracteres';
+      } else if(errorObj['email']) {
+        error = 'El email debe tener un formato válido';
+      } else if(errorObj['pattern']){
+        error = 'Debe ingresar un numero de teléfono válido'
+      }
+    }
+    return error;
+  }
 }
+
+
