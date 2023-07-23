f9b3ab4d781726de307c1b18fca44576756af2c5
vevis <vevisvs@gmail.com>
2023-07-22 22:10:27 -0300

    agregando user service y rxjs

--- src/app/board/pages/users/users.component.ts
+++ src/app/board/pages/users/users.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalUsersComponent } from './components/modal-users/modal-users.component';
 import { Users } from './models/users';
+import { UserService } from 'src/app/core/services/user.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-users',
@@ -10,27 +12,27 @@ import { Users } from './models/users';
 })
 export class UsersComponent {
 
-  public alumnos: Users[] = []
+  alumnos$: Observable<Users[]>;
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, private userService: UserService) {
+      this.alumnos$ = this.userService.getUsers();
+  };
 
   openDialog(): void {
     this.dialog.open(ModalUsersComponent).afterClosed().subscribe({
       next: (result) => {
         if(result){
           console.log(result)
-          this.alumnos = [
-            ...this.alumnos,
+          this.userService.addUser(
             {
-                id: Date.now() + Math.floor(Math.random() * 1000),
-                name: result.name,
-                lastname: result.lastname,
-                email: result.email,
-                country: result.country,
-                phone: result.phone
-            },
-          ]
-          console.log("alumnos:", this.alumnos)
+              id: Date.now() + Math.floor(Math.random() * 1000),
+              name: result.name,
+              lastname: result.lastname,
+              email: result.email,
+              country: result.country,
+              phone: result.phone
+            }
+          );
         } else{
           console.log("No se recibió ninguna información")
         }
@@ -42,21 +44,16 @@ export class UsersComponent {
     const dialogRef = this.dialog.open(ModalUsersComponent, { data: userToModify });
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        console.log('Usuario modificado:', result);
-        const updatedUserIndex = this.alumnos.findIndex(alumno => alumno.id === userToModify.id);
-        if (updatedUserIndex !== -1) {
-          this.alumnos[updatedUserIndex] = result;
-          this.alumnos = this.alumnos.slice();
-        }
-      } else {
-        console.log('Modificación de usuario cancelada');
-      }
+        this.userService.onEdit(result)
+        console.log("user edited:", result)
+      };
     });
   }
 
-  deleteUser(alumnoId: number): void{
-    this.alumnos = this.alumnos.filter(alumno => alumno.id != alumnoId);
+  deleteUser(userId: number): void{
+    this.userService.onDelete(userId);
   }
+
 
 }
 
---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Users } from 'src/app/board/pages/users/models/users';
+import { BehaviorSubject, take, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private users: Users[] = []
+
+  private users$: BehaviorSubject<Users[]> = new BehaviorSubject<Users[]>(this.users)
+
+  getUsers(): Observable<Users[]>{
+    return this.users$.asObservable();
+  }
+
+  addUser(user: Users): void{
+    this.users = [...this.users, user]
+    this.users$.next(this.users)
+  }
+
+  onEdit(userToModify: Users){
+    const index = this.users.findIndex(u => u.id === userToModify.id);
+    if (index !== -1) {
+      this.users[index] = { ...this.users[index], ...userToModify };
+      this.users$.next([...this.users]);
+    }
+  }
+
+  onDelete(userId: number){
+    const filteredUsers = this.users.filter((user) => user.id !== userId);
+    this.users = [...filteredUsers];
+    this.users$.next(filteredUsers);
+  }
+
+}
+
+
+
