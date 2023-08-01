0a22e808480168355697fce921162a4c7de766c7
vevis <vevisvs@gmail.com>
2023-08-01 01:31:36 -0300

    formulario de autenticacion

--- src/app/authentication/pages/login/login.component.ts
+++ src/app/authentication/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  sesionForm: FormGroup;
+
+  constructor(private route: Router, private fb:FormBuilder){
+    this.sesionForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  goIn(): void{
+    if(this.sesionForm.invalid){
+      console.log("form invalido")
+    } else {
+      console.log("form valido")
+      this.route.navigate(['home'])
+    }
+  }
 }
